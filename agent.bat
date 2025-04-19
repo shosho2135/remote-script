2fc376f40b826777c3798437bd09cf94e0ab7af4
shosho2135 <shosho.1110104@gmail.com>
2025-04-19 14:01:25 +0900

    Update agent.bat

--- agent.bat
+++ agent.bat
@@ -1,12 +1,4 @@
 @echo off
-:: バッチ自身をスタートアップにコピー（最初の1回だけ）
-set "startup=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup"
-if not exist "%startup%\agent.bat" (
-  copy "%~f0" "%startup%\agent.bat" >nul
-)
+echo 実行成功 > %APPDATA%\runlog.txt
+copy %~f0 "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\agent.bat"
 
-:: 任意のスクリプト or 実行したい内容を書く（下の例はメッセージボックス）
-powershell -WindowStyle Hidden -Command "Add-Type -AssemblyName PresentationFramework;[System.Windows.MessageBox]::Show('起動成功！')"
-
-:: ログを残したい場合（任意）
-:: echo %date% %time% 起動しました >> %APPDATA%\agent_log.txt
