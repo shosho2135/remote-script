e8f868d88d42270703dd2e7e3b941b665d98e53f
shosho2135 <shosho.1110104@gmail.com>
2025-04-21 12:40:57 +0900

    Update agent.bat

--- agent.bat
+++ agent.bat
@@ -1,1 +1,13 @@
+#include <Keyboard.h>
+
+void setup() {
+  delay(3000);  // PC起動待機
+  Keyboard.begin();
+
+  // 100%化けないUS配列用文字列送信
+  Keyboard.print("Pro Micro test123:/\\");  // ← 本命の1行
+  Keyboard.end();
+}
+
+void loop() {}
 
